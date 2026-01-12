e8f9617b0d3f41fbd272b15b7cc61a54e642bfe9
Kyle Bautista <143971638+KymaiselHunter@users.noreply.github.com>
2026-01-11 22:21:56 -0500

    ADD Minimum Time Visiting All Points

--- Minimum Time Visiting All Points/Minimum Time Visiting All Points.py
+++ Minimum Time Visiting All Points/Minimum Time Visiting All Points.py
@@ -1,23 +1,8 @@
 class Solution:
     def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
-        currX = points[0][0]
-        currY = points[0][1]
-
         time = 0
-        for i in range(1,len(points)):
-            while currX != points[i][0] or currY != points[i][1]:
-                # print(currX, currY)
-                if currX > points[i][0]:
-                    currX -= 1
-                elif currX < points[i][0]:
-                    currX +=1
-
-                if currY > points[i][1]:
-                    currY -= 1
-                elif currY < points[i][1]:
-                    currY +=1
-
-                time +=1 
+        for i in range(len(points)-1):
+            time += max(abs(points[i][0]-points[i+1][0]), abs(points[i][1]-points[i+1][1]))
             # print(i, time)
 
         return time
