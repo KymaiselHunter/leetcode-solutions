a8cf9c78cbf503562e126640c3482a7202d89027
Kyle Bautista <143971638+KymaiselHunter@users.noreply.github.com>
2026-01-14 01:01:02 -0500

    ADD K Closest Points to Origin

---
+++ K Closest Points to Origin/K Closest Points to Origin.py
@@ -0,0 +1,12 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        queue = []
+
+        for point in points:
+            dist = math.sqrt(pow(point[0], 2) + pow(point[1],2))
+            heapq.heappush(queue, (dist, point))
+
+            while len(queue) > k:
+                heapq.heappop(queue)
+
+        return queue
