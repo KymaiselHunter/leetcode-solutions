f789df1b0f3110cccd150917bdc009712384da6d
Kyle Bautista <143971638+KymaiselHunter@users.noreply.github.com>
2026-02-17 20:13:47 -0500

    ADD Binary Number with Alternating Bits

--- Binary Number with Alternating Bits/Binary Number with Alternating Bits.py
+++ Binary Number with Alternating Bits/Binary Number with Alternating Bits.py
@@ -1,12 +1,13 @@
 class Solution:
     def hasAlternatingBits(self, n: int) -> bool:
-        odd = True
-        while n:
+        odd = n % 2 
+        while n > 0:
             curr = n % 2 
             if odd and not curr:
                 return False
             if not odd and curr:
                 return False
+            # print(n, curr)
             n //= 2
             odd = not odd
         return True
