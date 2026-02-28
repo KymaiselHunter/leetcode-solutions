fa7f7152518fb73e65f51db6ed7261bb8f92fa6f
Kyle Bautista <143971638+KymaiselHunter@users.noreply.github.com>
2026-02-28 14:10:10 -0500

    ADD Concatenation of Consecutive Binary Numbers

--- Concatenation of Consecutive Binary Numbers/Concatenation of Consecutive Binary Numbers.py
+++ Concatenation of Consecutive Binary Numbers/Concatenation of Consecutive Binary Numbers.py
@@ -1,8 +1,13 @@
 class Solution:
     def concatenatedBinary(self, n: int) -> int:
-        output = str()
+        output = int()
+        bits = 1
 
         for i in range(1,n+1):
-            output += str(bin(i)[2:])
+            if pow(2, bits) <= i:
+                bits  += 1
 
-        return int(output,2) % (pow(10,9) + 7)
+            output = output << bits
+            output = output | i
+
+        return output % (pow(10,9) + 7)
