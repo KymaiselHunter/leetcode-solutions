a3653650565383063a431366b02a2fdee22468e8
Kyle Bautista <143971638+KymaiselHunter@users.noreply.github.com>
2026-03-02 15:18:44 -0500

    ADD Top K Frequent Elements

---
+++ Top K Frequent Elements/Top K Frequent Elements.py
@@ -0,0 +1,8 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        c = Counter(nums)
+
+        # print(sorted(c.items(), key=lambda item: item[1]))
+        top = sorted(c.items(), key=lambda item: item[1], reverse=True)
+
+        return [top[i][0] for i in range(k)]
