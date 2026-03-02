4318a29353ad0855ce9c581d6f25177cbf463a9b
Kyle Bautista <143971638+KymaiselHunter@users.noreply.github.com>
2026-03-02 15:28:58 -0500

    ADD Top K Frequent Elements

--- Top K Frequent Elements/Top K Frequent Elements.py
+++ Top K Frequent Elements/Top K Frequent Elements.py
@@ -3,6 +3,10 @@ class Solution:
         c = Counter(nums)
 
         # print(sorted(c.items(), key=lambda item: item[1]))
-        top = sorted(c.items(), key=lambda item: item[1], reverse=True)
+        # top = sorted(c.items(), key=lambda item: item[1], reverse=True)
 
-        return [top[i][0] for i in range(k)]
+        return heapq.nlargest(k, c.keys() , key=lambda item: c[item])
+
+        # # print(heap)
+
+        # return [top[i][0] for i in range(k)]
