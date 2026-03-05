ef071461ac53bfe91b040b4549c8649885cf7092
Kyle Bautista <143971638+KymaiselHunter@users.noreply.github.com>
2026-03-04 20:14:30 -0500

    ADD Minimum Changes To Make Alternating Binary String

--- Minimum Changes To Make Alternating Binary String/Minimum Changes To Make Alternating Binary String.py
+++ Minimum Changes To Make Alternating Binary String/Minimum Changes To Make Alternating Binary String.py
@@ -2,9 +2,23 @@ class Solution:
     def minOperations(self, s: str) -> int:
         out = 0
 
-        ones = 0
-        for c in s:
-            if c == '1':
-                ones += 1
+        startOneInc = 0
+        startZeroInc = 0
+        for i in range(len(s)):
+            if i%2==0:
+                if s[i] == '1':
+                    startZeroInc += 1
 
-        return min(abs(len(s)//2 - ones), abs((len(s)//2) - (len(s)-ones)))
+                if s[i] == '0':
+                    startOneInc += 1
+                continue
+            
+            if s[i] != '1':
+                startZeroInc += 1
+
+            if s[i] != '0':
+                startOneInc += 1
+                
+
+
+        return min(startOneInc, startZeroInc)
