cccd6621c2b6fe9d1e26b52dacb7d7ffbca41249
Kyle Bautista <143971638+KymaiselHunter@users.noreply.github.com>
2026-03-17 17:39:48 -0400

    ADD Employee Bonus

---
+++ Employee Bonus/Employee Bonus.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+Select 
+    name,
+    bonus
+        FROM Employee e
+        LEFT JOIN Bonus b
+            on e.empId = b.empId
+            WHERE (
+                b.bonus < 1000
+                OR
+                b.bonus IS NULL
+            );
