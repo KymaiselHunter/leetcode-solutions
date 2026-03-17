377226d4b5361bb52a93b88103c5d838123bfd9c
Kyle Bautista <143971638+KymaiselHunter@users.noreply.github.com>
2026-03-17 17:26:41 -0400

    ADD Game Play Analysis I

--- Game Play Analysis I/Game Play Analysis I.sql
+++ Game Play Analysis I/Game Play Analysis I.sql
@@ -1,10 +1,4 @@
 # Write your MySQL query statement below
-Select player_id, event_date as first_login
+Select player_id, min(event_date) as first_login
     FROM Activity
-    WHERE (player_id, event_date) NOT IN(
-        SELECT A.player_id, A.event_date
-        FROM Activity A
-        JOIN Activity B
-            ON A.player_id = B.player_id
-            AND A.event_date > B.event_date
-        );
+    GROUP BY player_id;
