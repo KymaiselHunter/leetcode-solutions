241da24c158c606d72cd1b990f22480150a9dffa
Kyle Bautista <143971638+KymaiselHunter@users.noreply.github.com>
2026-03-20 12:25:54 -0400

    ADD Number of 1 Bits

---
+++ Number of 1 Bits/Number of 1 Bits.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        out = 0
+        while n:
+            if n % 2 == 1:
+                out += 1
+            n//=2
+        return out
