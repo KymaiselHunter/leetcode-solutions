861a22ee637aa9de8053e50f1f071414f81099b2
Kyle Bautista <143971638+KymaiselHunter@users.noreply.github.com>
2026-03-21 11:47:14 -0400

    ADD Increasing Triplet Subsequence

--- Increasing Triplet Subsequence/Increasing Triplet Subsequence.py
+++ Increasing Triplet Subsequence/Increasing Triplet Subsequence.py
@@ -1,21 +1,21 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        biggest = []
-        for j in range(len(nums)-1,0,-1):
-            if not biggest:
-                biggest.insert(0, nums[j])
+        biggest = [-1 for j in range(len(nums))]
+        for j in range(len(nums)-1,-1,-1):
+            if j == len(nums)-1:
+                biggest[j] = nums[j]
                 continue
             curr = max(nums[j], biggest[0])
-            biggest.insert(0, curr)
+            biggest[j]=curr
 
         left = nums[0]
-        for i in range(1, len(nums)):
-            if left < nums[i] and nums[i] < biggest[0]:
+        for i in range(1, len(nums)-1):
+            if left < nums[i] and nums[i] < biggest[i+1]:
                 return True
             
             if left > nums[i]:
                 left = nums[i]
-            biggest.pop(0)
+            # biggest.pop(0)
 
         return False
                 
