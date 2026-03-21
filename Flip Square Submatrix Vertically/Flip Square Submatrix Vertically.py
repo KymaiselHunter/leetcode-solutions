43b2cff97cfecd9f2b2567cdc06df047108a8695
Kyle Bautista <143971638+KymaiselHunter@users.noreply.github.com>
2026-03-21 09:57:28 -0400

    ADD Flip Square Submatrix Vertically

--- Flip Square Submatrix Vertically/Flip Square Submatrix Vertically.py
+++ Flip Square Submatrix Vertically/Flip Square Submatrix Vertically.py
@@ -2,7 +2,7 @@ class Solution:
     def reverseSubmatrix(self, grid: List[List[int]], x: int, y: int, k: int) -> List[List[int]]:
         for j in range(y, y+k):
             print(j)
-            for i in range((x+k)//2):
+            for i in range((k)//2):
                 grid[x+i][j], grid[(x+k-1)-i][j] = grid[(x+k-1)-i][j], grid[x+i][j]
 
         return grid
