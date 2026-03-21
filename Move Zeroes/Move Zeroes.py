51339e030f8db21bcc133709bd9a4511284a383b
Kyle Bautista <143971638+KymaiselHunter@users.noreply.github.com>
2026-03-21 15:12:41 -0400

    ADD Move Zeroes

--- Move Zeroes/Move Zeroes.py
+++ Move Zeroes/Move Zeroes.py
@@ -3,17 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        zeros = list()
-
+        left = 0
         for i in range(len(nums)):
-            if nums[i] == 0:
-                zeros.append(i)
-
-        for i in range(len(zeros)):
-            bound = zeros[i+1] if i < len(zeros)-1 else len(nums)
-            
-            for j in range(zeros[i]+1, bound):
-                nums[j-(i+1)] = nums[j]
-        
-        for i in range(len(nums) - len(zeros) , len(nums)):
-            nums[i] = 0
+            if nums[i] != 0:
+                nums[i], nums[left] = nums[left], nums[i]
+                left += 1
