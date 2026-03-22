177ee9a42048b356b45f03726e9a831de9a623fe
Kyle Bautista <143971638+KymaiselHunter@users.noreply.github.com>
2026-03-22 12:27:26 -0400

    ADD Minimum Removals to Achieve Target XOR

--- Minimum Removals to Achieve Target XOR/Minimum Removals to Achieve Target XOR.py
+++ Minimum Removals to Achieve Target XOR/Minimum Removals to Achieve Target XOR.py
@@ -4,17 +4,23 @@ class Solution:
 
         def backtrack(i, total, included):
             print(i, total, included)
-            if i == len(nums):
-                return included if total == target else -1
             if (i, total, included) in dp:
+                print(i, total, included,"are you fucking ")
+                return dp[(i,total,included)]
+            if i == len(nums):
+                dp[(i,total,included)] = included if total == target else -1#, dp[(i,total,included)] if dp[(i,total,included)] else -1)
                 return dp[(i,total,included)]
             
-            dp[(i+1, total, included)] = backtrack(i+1, total, included)
-            dp[(i+1, total, included+1)] = backtrack(i+1, total ^ nums[i], included+1)
+            ignore = backtrack(i+1, total, included)
+            # dp[(i, total, included)] = ignore
+            take = backtrack(i+1, total ^ nums[i], included+1)
+            # dp[(i, total, included+1)] = take
+            dp[(i, total, included)] = max(take, ignore)
+            return dp[(i, total, included)]
         
-        backtrack(0,0,0)
+        res=backtrack(0,0,0)
         # print(dp)
         # print(max(v for v in dp.values() if v is not None))
-        # print(max(dp, key=lambda x: x[2]))
-        included = max(v for v in dp.values() if v is not None)
-        return len(nums) - included if included > -1 else included 
+        # # print(max(dp, key=lambda x: x[2]))
+        # included = max(v for v in dp.values() if v is not None)
+        return len(nums) - res if res > -1 else res
