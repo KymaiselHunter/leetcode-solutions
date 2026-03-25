d6c5708a2daea552adf7ab8ab7f513aab7c4c04c
Kyle Bautista <143971638+KymaiselHunter@users.noreply.github.com>
2026-03-25 19:23:37 -0400

    ADD Equal Sum Grid Partition I

--- Equal Sum Grid Partition I/Equal Sum Grid Partition I.py
+++ Equal Sum Grid Partition I/Equal Sum Grid Partition I.py
@@ -10,25 +10,25 @@ class Solution:
 
         shrink = totalSum
         grow = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[i])-1):
+        for i in range(len(grid)-1):
+            for j in range(len(grid[i])):
                 grow += grid[i][j]
                 shrink -= grid[i][j]
 
-
             if shrink == grow:
-                # print("vertical at", i)
+                # print("horizontal at", i)
                 return True
         
         shrink = totalSum
         grow = 0
-        for j in range(len(grid[0])):
-            for i in range(len(grid)-1):
+        for j in range(len(grid[0])-1):
+            for i in range(len(grid)):
                 grow += grid[i][j]
                 shrink -= grid[i][j]
             
+            # print(shrink,grow)
             if shrink == grow:
-                # print("horizontal at", j)
+                # print("vertical at", j)
                 return True
 
         return False
