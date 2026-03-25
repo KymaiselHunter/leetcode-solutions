8066706483e40bc950d5ec2c8d0334fee15dd8c9
Kyle Bautista <143971638+KymaiselHunter@users.noreply.github.com>
2026-03-25 19:18:21 -0400

    ADD Equal Sum Grid Partition I

---
+++ Equal Sum Grid Partition I/Equal Sum Grid Partition I.py
@@ -0,0 +1,36 @@
+class Solution:
+    def canPartitionGrid(self, grid: List[List[int]]) -> bool:
+        totalSum = 0
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                totalSum += grid[i][j]
+
+        # print("totalSum:", totalSum)
+
+        shrink = totalSum
+        grow = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[i])-1):
+                grow += grid[i][j]
+                shrink -= grid[i][j]
+
+
+            if shrink == grow:
+                # print("vertical at", i)
+                return True
+        
+        shrink = totalSum
+        grow = 0
+        for j in range(len(grid[0])):
+            for i in range(len(grid)-1):
+                grow += grid[i][j]
+                shrink -= grid[i][j]
+            
+            if shrink == grow:
+                # print("horizontal at", j)
+                return True
+
+        return False
+
+        
