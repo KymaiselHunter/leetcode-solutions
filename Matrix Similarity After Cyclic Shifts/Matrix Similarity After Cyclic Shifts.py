bc75b4c3b8aeafb00bcf46b5885faa9b05c6141e
Kyle Bautista <143971638+KymaiselHunter@users.noreply.github.com>
2026-03-27 14:23:14 -0400

    ADD Matrix Similarity After Cyclic Shifts

---
+++ Matrix Similarity After Cyclic Shifts/Matrix Similarity After Cyclic Shifts.py
@@ -0,0 +1,19 @@
+class Solution:
+    def areSimilar(self, mat: List[List[int]], k: int) -> bool:
+        k = k % len(mat)
+        rotMat = copy.deepcopy(mat)
+        for rot in range(k):
+            for row in range(len(mat)):
+                even = row % 2 == 0
+
+                if even:
+                    rotMat[row].append(rotMat[row].pop(0))
+                else:
+                    rotMat[row].insert(0, rotMat[row].pop(-1))
+
+        # print(rotMat)
+        # print(mat)
+        if mat == rotMat:
+            return True
+
+        return False
