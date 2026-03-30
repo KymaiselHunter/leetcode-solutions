f13015298987ec0a784d0ca209f35dd4e964a737
Kyle Bautista <143971638+KymaiselHunter@users.noreply.github.com>
2026-03-30 16:55:40 -0400

    ADD Check if Strings Can be Made Equal With Operations I

---
+++ Check if Strings Can be Made Equal With Operations I/Check if Strings Can be Made Equal With Operations I.py
@@ -0,0 +1,21 @@
+class Solution:
+    def canBeEqual(self, s1: str, s2: str) -> bool:
+        cEven = Counter(s1[0::2])
+        cOdd = Counter(s1[1::2])
+
+        # print(cEven)
+        # print(cOdd)
+
+        for index, char in enumerate(s2):
+            c = cEven if index % 2 == 0 else cOdd 
+            if char not in c: 
+                return False
+
+            if c[char] <= 0:
+                return False
+
+            c[char] -= 1
+        
+        # print(cEven)
+        # print(cOdd)
+        return True
