1cd7a02d44a03b3c6482c31c470050a45a8ea414
Kyle Bautista <143971638+KymaiselHunter@users.noreply.github.com>
2026-04-08 13:51:16 -0400

    ADD Find the Difference of Two Arrays

---
+++ Find the Difference of Two Arrays/Find the Difference of Two Arrays.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        set1 = set(nums1)
+        set2 = set(nums2)
+
+        intersection = set1.intersection(set2)
+        # print(intersection)
+
+        return [list(set1-intersection),(list(set2-intersection))]
