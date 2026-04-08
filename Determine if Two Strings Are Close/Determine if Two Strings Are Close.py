ea50394d0391d3cd62c27d4b0ca243efec21558f
Kyle Bautista <143971638+KymaiselHunter@users.noreply.github.com>
2026-04-08 14:09:12 -0400

    ADD Determine if Two Strings Are Close

--- Determine if Two Strings Are Close/Determine if Two Strings Are Close.py
+++ Determine if Two Strings Are Close/Determine if Two Strings Are Close.py
@@ -3,12 +3,12 @@ class Solution:
         c1 = Counter(word1)
         c2 = Counter(word2)
 
-        vals1 = list(c1.values())
-        vals2 = list(c2.values())
+        vals1 = sorted(list(c1.values()))
+        vals2 = sorted(list(c2.values()))
 
         chars1 = set(word1)
         chars2 = set(word2)
 
+        return vals1 == vals2 and chars1 == chars2 
         # print(vals1)
         # print(vals2)
-        return vals1 == vals2 and chars1 == chars2 
