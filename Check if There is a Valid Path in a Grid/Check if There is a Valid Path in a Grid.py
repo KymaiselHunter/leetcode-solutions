1d1c5c7aa665dca7dd5c321a7441cd91c480bc4d
Kyle Bautista <143971638+KymaiselHunter@users.noreply.github.com>
2026-04-27 18:28:55 -0400

    ADD Check if There is a Valid Path in a Grid

---
+++ Check if There is a Valid Path in a Grid/Check if There is a Valid Path in a Grid.py
@@ -0,0 +1,45 @@
+class Solution:
+    def hasValidPath(self, grid: List[List[int]]) -> bool:
+        DIRS = ((1,0), (-1, 0), (0,1), (0,-1))
+        streets = {
+            1 : ((0,1), (0,-1)),
+            2 : ((1,0), (-1,0)),
+            3 : ((0,-1), (1,0)),
+            4 : ((0,1), (1,0)),
+            5 : ((0,-1), (-1,0)),
+            6 : ((0,1), (-1,0)),
+        }
+
+        def inBounds(row:int,col:int, grid: List[List[int]]) -> bool:
+            if row < 0 or col < 0:
+                return False
+            if row >= len(grid) or col >= len(grid[row]): 
+                return False
+            return True
+
+        passed = set() 
+        bfs = [(len(grid)-1, len(grid[0])-1)]
+
+        while bfs:
+            currRow, currCol = bfs.pop(0)
+            if (currRow, currCol) in passed:
+                continue
+
+            if (currRow, currCol) == (0,0):
+                return True
+
+            passed.add((currRow, currCol))
+
+            streetType = grid[currRow][currCol]
+            ahead = streets[streetType]
+
+            for dr,dc in ahead:
+                nextRow, nextCol = currRow + dr, currCol + dc
+                if not inBounds(nextRow, nextCol, grid):
+                    continue
+                if (nextRow, nextCol) in passed:
+                    continue
+                bfs.append((currRow + dr, currCol + dc))
+
+        return False
+                
