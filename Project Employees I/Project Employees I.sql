41b359fd2480f7c3ce4d8cfb70d56ec1754baace
Kyle Bautista <143971638+KymaiselHunter@users.noreply.github.com>
2026-05-20 18:26:58 -0400

    ADD Project Employees I

--- Project Employees I/Project Employees I.sql
+++ Project Employees I/Project Employees I.sql
@@ -2,7 +2,7 @@
 SELECT 
     -- *,
     project_id,
-    AVG(experience_years) AS average_years
+    ROUND(AVG(experience_years), 2) AS average_years
 FROM Project AS p
 JOIN Employee AS e
 ON e.employee_id = p.employee_id
