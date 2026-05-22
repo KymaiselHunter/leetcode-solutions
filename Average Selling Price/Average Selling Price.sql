ceb8b91b642e331da1333042666077fd9c2a5967
Kyle Bautista <143971638+KymaiselHunter@users.noreply.github.com>
2026-05-22 14:43:17 -0400

    ADD Average Selling Price

--- Average Selling Price/Average Selling Price.sql
+++ Average Selling Price/Average Selling Price.sql
@@ -4,10 +4,10 @@ SELECT
     p.product_id,
     -- price * units,
     -- SUM(units),
-    ROUND(SUM(p.price * u.units) / SUM(units), 2) AS average_price
+    IFNULL(ROUND(SUM(p.price * u.units) / SUM(units), 2), 0) AS average_price
     -- price * units / SUM(units)
 FROM Prices AS p
-JOIN UnitsSold AS u
+LEFT JOIN UnitsSold AS u
 ON purchase_date BETWEEN start_date AND end_date
 AND p.product_id = u.product_id
 GROUP BY p.product_id;
