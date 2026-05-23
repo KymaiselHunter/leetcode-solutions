e0643cfd5682963db75d34815dbb969e67c41988
Kyle Bautista <143971638+KymaiselHunter@users.noreply.github.com>
2026-05-23 14:07:03 -0400

    ADD 3Sum

--- 3Sum/3Sum.py
+++ 3Sum/3Sum.py
@@ -1,8 +1,9 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
+        nums = sorted(nums)
         cache = set()
         cache.add(nums[0])
-        out = set()
+        out = list()
 
         for i in range(1,len(nums)):
             skip = set()
@@ -10,12 +11,10 @@ class Solution:
                 if (nums[i], nums[j]) in skip:
                     continue
                 if (nums[i] + nums[j]) * -1 in cache:
-                    new = [nums[i], nums[j], (nums[i] + nums[j]) * -1]
-                    new = tuple(sorted(new))
-                    out.add(new)
+                    new = [(nums[i] + nums[j]) * -1, nums[i], nums[j]]
+                    out.append(new)
                     skip.add((nums[i], nums[j]))
             cache.add(nums[i])
 
-        out = [list(item) for item in out]
 
         return out
