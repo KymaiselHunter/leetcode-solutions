18e69603db33a9cdc4036dfabbb8609d0f2d82bc
Kyle Bautista <143971638+KymaiselHunter@users.noreply.github.com>
2026-05-27 14:30:48 -0400

    ADD Queries Quality and Percentage

--- Queries Quality and Percentage/Queries Quality and Percentage.sql
+++ Queries Quality and Percentage/Queries Quality and Percentage.sql
@@ -4,6 +4,6 @@ SELECT
     -- SUM(rating/position) / Count(*) AS quality
     query_name,
     ROUND(AVG(rating/position), 2) AS quality,
-    ROUND((SUM(rating < 3) / COUNT(*)) * 100, 2) AS poor_query_percentage
+    ROUND(AVG(rating < 3) * 100, 2) AS poor_query_percentage
 FROM Queries
 GROUP BY query_name;
