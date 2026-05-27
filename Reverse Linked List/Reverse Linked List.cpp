0bed3a08ae891e9edd8889519ced3ce382ad4878
Kyle Bautista <143971638+KymaiselHunter@users.noreply.github.com>
2026-05-27 16:35:14 -0400

    ADD Reverse Linked List

---
+++ Reverse Linked List/Reverse Linked List.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(!head || !head->next) return head;
+        
+        ListNode* prev = head;
+        ListNode* next = head->next;
+        head->next = nullptr;
+        
+        while(prev && next)
+        {
+            cout << prev->val << endl;
+            ListNode * hold = next->next;
+            next->next = prev;
+            prev = next;
+            next = hold; 
+        }
+
+        return prev;
+    }
+};
