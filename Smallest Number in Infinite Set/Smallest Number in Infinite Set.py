f7f1ed255cb18cc5fa99df58a52ddbd310d9a69d
Kyle Bautista <143971638+KymaiselHunter@users.noreply.github.com>
2026-06-01 15:23:38 -0400

    ADD Smallest Number in Infinite Set

---
+++ Smallest Number in Infinite Set/Smallest Number in Infinite Set.py
@@ -0,0 +1,24 @@
+class SmallestInfiniteSet:
+
+    def __init__(self):
+        self.h = list()
+        self.n = 1
+
+    def popSmallest(self) -> int:
+        if not self.h:
+            out = self.n
+            self.n += 1
+            return out
+        
+        return heapq.heappop(self.h)
+
+    def addBack(self, num: int) -> None:
+        if num >= self.n:
+            return
+        heapq.heappush(self.h, num)
+
+
+# Your SmallestInfiniteSet object will be instantiated and called as such:
+# obj = SmallestInfiniteSet()
+# param_1 = obj.popSmallest()
+# obj.addBack(num)
