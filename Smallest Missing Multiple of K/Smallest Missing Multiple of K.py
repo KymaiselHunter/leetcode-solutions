aaa009ff04b5fa3a9e8fcff8d2097edd7695d101
Kyle Bautista <143971638+KymaiselHunter@users.noreply.github.com>
2026-08-25 12:46:20 -0400

    ADD Smallest Missing Multiple of K

---
+++ Smallest Missing Multiple of K/Smallest Missing Multiple of K.py
@@ -0,0 +1,10 @@
+class Solution:
+    def missingMultiple(self, nums: List[int], k: int) -> int:
+        check = set(nums)
+
+
+        for i in range(k, 101, k):
+            if not i in check:
+                return i
+
+        return -1
