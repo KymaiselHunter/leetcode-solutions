4b901474c4eb65013c6444c9218996b66aebd5f6
Kyle Bautista <143971638+KymaiselHunter@users.noreply.github.com>
2026-09-07 10:24:34 -0400

    ADD Longest Substring Without Repeating Characters

--- Longest Substring Without Repeating Characters/Longest Substring Without Repeating Characters.py
+++ Longest Substring Without Repeating Characters/Longest Substring Without Repeating Characters.py
@@ -1,17 +1,17 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        left = 0
+        left = 0 
         d = dict()
         out = 0
 
         for i, c in enumerate(s):
-            # print(i, c, out, d)
+            # print(i,, c, out, d)
             if c not in d:
                 d[c] = i
                 out = max(out, i - left + 1)
                 continue
-            
-            left = max(left, d[c] + 1)
+
+            left = d[c] + 1
             d[c] = i
 
             out = max(out, i - left + 1)
