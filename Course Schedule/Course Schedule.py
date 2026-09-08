0cab99b6162f3d9c23aeae86a94688afe1db8465
Kyle Bautista <143971638+KymaiselHunter@users.noreply.github.com>
2026-09-08 13:06:38 -0400

    ADD Course Schedule

--- Course Schedule/Course Schedule.py
+++ Course Schedule/Course Schedule.py
@@ -1,22 +1,23 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        mp = [[] for i in range(numCourses)]
+
+        for i in range(len(prerequisites)):
+            mp[prerequisites[i][0]].append(prerequisites[i][1])
+
         def recur(dfs, prerequisites) -> bool:
-            # print(dfs)
-            for i in range(len(prerequisites)):
-                if prerequisites[i][1] != dfs[-1]:
-                    continue
-                # print(prerequisites[i][0])
-                if prerequisites[i][0] in dfs:
+            for num in mp[dfs[-1]]:
+                if num in dfs:
                     return False
                 
-                dfs.append(prerequisites[i][0])
+                dfs.append(num)
                 if not recur(dfs, prerequisites):
                     return False
                 dfs.pop(-1)
             return True
 
-        for i in range(len(prerequisites)):
-            dfs = [prerequisites[i][1]]
+        for i in range(numCourses):
+            dfs = [i]
             if not recur(dfs, prerequisites):
                 return False
 
