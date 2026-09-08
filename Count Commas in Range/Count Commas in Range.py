b00cdbeff014e99e907a57640743e31edb274085
Kyle Bautista <143971638+KymaiselHunter@users.noreply.github.com>
2026-09-08 12:05:27 -0400

    ADD Count Commas in Range

---
+++ Count Commas in Range/Count Commas in Range.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countCommas(self, n: int) -> int:
+        if n < 1000:
+            return 0
+
+        small = n % 1000
+        big = n // 1000
+
+        return (100 * (big - 1)) + (small + 1)
