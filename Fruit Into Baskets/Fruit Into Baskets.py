04424507c81c428ee4140fc9f5880e52c0a7b092
Kyle Bautista <143971638+KymaiselHunter@users.noreply.github.com>
2026-09-11 13:38:24 -0400

    ADD Fruit Into Baskets

--- Fruit Into Baskets/Fruit Into Baskets.py
+++ Fruit Into Baskets/Fruit Into Baskets.py
@@ -18,7 +18,9 @@ class Solution:
             # if there's more than two fruits, remove
             if len(d) > 2:
                 # if there's more than two fruits, remove the third
-                rightFruit = max(d.items(), key=lambda item: item[1])[0]
+                rightFruit = max(d.items(), key=lambda item: item[1][1])[0]
+                # print(d.items())
+                # print(rightFruit)
                 d.pop(rightFruit)
 
                 # update the fruit that wasnt removed
@@ -29,6 +31,6 @@ class Solution:
             
             # if there's only two fruits, we should add up
             out = max(out, sum(k[0] for k in d.values()))
-            # print(out, d)
+            print(out, d)
 
         return out
